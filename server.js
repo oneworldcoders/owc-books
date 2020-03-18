41ce40b82c226252d69a8e6109b1b2fbfd9af9ac
Henry Okonkwo <okonkwo.henry2012@gmail.com>
2020-03-18 15:54:11 +0200

    setup linter

--- server.js
+++ server.js
@@ -1,17 +1,16 @@
-const express = require('express');
-const path = require('path');
-const favicon = require('express-favicon');
-
+const express = require("express");
+const path = require("path");
+const favicon = require("express-favicon");
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(favicon(__dirname + '/build/favicon.ico'));
+app.use(favicon(__dirname + "/build/favicon.ico"));
 app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, "build")));
 
-app.get('/*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+app.get("/*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "build", "index.html"));
 });
 
 app.listen(port, () => {
