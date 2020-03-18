216de0a8ece01d32ea2c6533100fd46e758a4142
Henry Okonkwo <okonkwo.henry2012@gmail.com>
2020-03-18 15:23:59 +0200

    host owc books app on heroku configure express app set up a node server

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+const favicon = require('express-favicon');
+
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(favicon(__dirname + '/build/favicon.ico'));
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Up and running on port ${port}`);
+});
